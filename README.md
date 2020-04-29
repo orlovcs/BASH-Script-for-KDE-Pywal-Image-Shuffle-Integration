66fad3216f7c1ffd5fbcf3706fb14340f69bad5e
Nick <orlov.a.nikita@gmail.com>
2020-04-29 00:28:08 -0400

    added a nicer readme and usage instructions, updated args

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
 # BASH-Script-for-KDE-Pywal-Image-Shuffle-Integration
 
-Needs:
-- https://github.com/dylanaraps/pywal
-- https://github.com/actionless/oomox
-Run this script with updated directories in order to:
-- copy a random image to the new directory
-- call pywal and oomox
-- make kde set it's wallpaper to the new image
+
+## Table of Contents
+
+* [About the Project](#about-the-project)
+  * [Built With](#built-with)
+  * [Prerequisites](#prerequisites)
+* [Usage](#usage)
+
+
+## About The Project
+
+This is a CLI tool to allow setting a KDE wallpaper and a custom terminal theme based on this wallpaper with a provided path.
+
+### Built With
+
+* []() PyWal
+
+### Prerequisites
+
+Install the following packages.
+
+* PyWal (https://github.com/dylanaraps/pywal)
+
+## Usage
+
+Clone and execute ```kdecolor.sh ```with:
+
+One command line argument specifies the path for one image which will be set as the new wallpaper and terminal theme source. 
+```sh
+./kdecolor.sh "/home/user/wallpapers/wallpaper.x"
+```
+
+Two command line arguments specify the path for one image from the first path to be copied over to the second path which will then be set as the new wallpaper and terminal theme source. 
+```sh
+./kdecolor.sh "/home/user/wallpapers_green/wallpaper.x" "/home/user/wallpapers_very_green/wallpaper.x"
+```
--- b.sh
+++ b.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
-drive="/run/media/human/Aka/Google Drive/Wallpapers"
-randm=$(ls "$drive" | shuf -n 1)
-fullpath=$(echo "$drive/$randm")
-newpath=$(echo "/home/human/Wallpaper/$randm")
-echo $fullpath
-cp -rf "$fullpath" "$newpath"
-wal -gi "$newpath"
-dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
-var Desktops = desktops();
- //         /run/media/human/Aka/Google Drive/Wallpapers/1513489791840.jpg                                                                                                          
-for (i=0;i<Desktops.length;i++) {
-        d = Desktops[i];
-        d.wallpaperPlugin = "org.kde.image";
-        d.currentConfigGroup = Array("Wallpaper",
-                                    "org.kde.image",
-                                    "General");
-        d.writeConfig("Image", "'"$newpath"'");
-}'
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+elif [ $# -eq 1 ]; then
+        echo "One path supplied"
+        echo "Shuffling image from path"
+        drive="$1"
+        randm=$(ls "$drive" | shuf -n 1)
+        fullpath=$(echo "$drive/$randm")
+        echo "Setting wallpaper to shuffled image from path:"
+        echo $fullpath
+        dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+        var Desktops = desktops();
+        for (i=0;i<Desktops.length;i++) {
+                d = Desktops[i];
+                d.wallpaperPlugin = "org.kde.image";
+                d.currentConfigGroup = Array("Wallpaper",
+                                        "org.kde.image",
+                                        "General");
+                d.writeConfig("Image", "'"$fullpath"'");
+        }'
+        echo "Switching terminal colors to new image"
+        wal -i "$newpath"
+elif [ $# -eq 2 ]; then
+        echo "Two paths supplied"
+        echo "Shuffling image from first path"
+        drive="$1"
+        randm=$(ls "$drive" | shuf -n 1)
+        fullpath=$(echo "$drive$randm")
+        newpath=$(echo "$2$randm")
+        echo "Copying over and setting wallpaper to shuffled image from path:"
+        echo $fullpath
+        cp -rf "$fullpath" "$newpath"
+        dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+        var Desktops = desktops();
+        for (i=0;i<Desktops.length;i++) {
+                d = Desktops[i];
+                d.wallpaperPlugin = "org.kde.image";
+                d.currentConfigGroup = Array("Wallpaper",
+                                        "org.kde.image",
+                                        "General");
+                d.writeConfig("Image", "'"$newpath"'");
+        }'
+        echo "Switching terminal colors to new image"
+        wal -i "$newpath"
+fi
+
+
+
 
